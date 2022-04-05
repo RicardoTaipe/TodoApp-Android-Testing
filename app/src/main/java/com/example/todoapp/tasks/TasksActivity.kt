4ee19b224e50b23d9ba6d17265689515e50ee13b
Ricardo Taipe <rdavid28@hotmail.es>
2022-04-04 21:09:40 -0500

    update ui main activity

--- app/src/main/java/com/example/todoapp/tasks/TasksActivity.kt
+++ app/src/main/java/com/example/todoapp/tasks/TasksActivity.kt
@@ -3,12 +3,12 @@ package com.example.todoapp.tasks
 import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.*
 import com.example.todoapp.R
 import com.example.todoapp.databinding.ActivityMainBinding
 
@@ -16,29 +16,30 @@ class TasksActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        drawerLayout = binding.drawerLayout
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
-        val navController = navHostFragment.navController
-        appBarConfiguration =
-            AppBarConfiguration.Builder(navController.graph).setOpenableLayout(binding.drawerLayout)
-                .build()
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        setupWithNavController(binding.navView, navController)
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
         setupNavigationDrawer()
     }
 
     private fun setupNavigationDrawer() {
-        binding.drawerLayout.setStatusBarBackground(R.color.purple_500)
+        drawerLayout.setStatusBarBackground(R.color.purple_500)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment_content_main).navigateUp(appBarConfiguration)
+        return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
 }
