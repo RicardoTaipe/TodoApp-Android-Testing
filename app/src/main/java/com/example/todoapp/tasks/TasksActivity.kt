f3e60de5e4853d83d659fa9ea2d25551b5745dd2
Ricardo Taipe <rdavid28@hotmail.es>
2022-04-04 19:56:48 -0500

    databinding updates in main activity

--- app/src/main/java/com/example/todoapp/tasks/TasksActivity.kt
+++ app/src/main/java/com/example/todoapp/tasks/TasksActivity.kt
@@ -1,15 +1,11 @@
 package com.example.todoapp.tasks
 
+import android.app.Activity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.NavigationUI.navigateUp
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -20,40 +16,34 @@ class TasksActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
-        drawerLayout = binding.drawerLayout
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph , drawerLayout)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(navController.graph).setOpenableLayout(binding.drawerLayout)
+                .build()
         setupActionBarWithNavController(navController, appBarConfiguration)
         setupWithNavController(binding.navView, navController)
+        setupNavigationDrawer()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun setupNavigationDrawer() {
+        binding.drawerLayout.setStatusBarBackground(R.color.purple_500)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navigateUp(navController, drawerLayout)
+        return findNavController(R.id.nav_host_fragment_content_main).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
+
+// Keys for navigation
+const val ADD_EDIT_RESULT_OK = Activity.RESULT_FIRST_USER + 1
+const val DELETE_RESULT_OK = Activity.RESULT_FIRST_USER + 2
+const val EDIT_RESULT_OK = Activity.RESULT_FIRST_USER + 3
---
+++ app/src/main/java/com/example/todoapp/util/ViewExt.kt
@@ -0,0 +1,51 @@
+package com.example.todoapp.util
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import com.example.todoapp.Event
+import com.example.todoapp.R
+import com.example.todoapp.ScrollChildSwipeRefreshLayout
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * Transforms static java function Snackbar.make() to an extension function on View.
+ */
+fun View.showSnackbar(snackbarText: String, timeLength: Int) {
+    Snackbar.make(this, snackbarText, timeLength).run {
+        show()
+    }
+}
+
+/**
+ * Triggers a snackbar message when the value contained by snackbarTaskMessageLiveEvent is modified.
+ */
+fun View.setupSnackbar(
+    lifecycleOwner: LifecycleOwner,
+    snackbarEvent: LiveData<Event<Int>>,
+    timeLength: Int
+) {
+
+    snackbarEvent.observe(lifecycleOwner) { event ->
+        event.getContentIfNotHandled()?.let {
+            showSnackbar(context.getString(it), timeLength)
+        }
+    }
+}
+
+fun Fragment.setupRefreshLayout(
+    refreshLayout: ScrollChildSwipeRefreshLayout,
+    scrollUpChild: View? = null
+) {
+    refreshLayout.setColorSchemeColors(
+        ContextCompat.getColor(requireActivity(), R.color.purple_500),
+        ContextCompat.getColor(requireActivity(), R.color.teal_200),
+        ContextCompat.getColor(requireActivity(), R.color.purple_700)
+    )
+    // Set the scrolling view in the custom SwipeRefreshLayout.
+    scrollUpChild?.let {
+        refreshLayout.scrollUpChild = it
+    }
+}
