b4977ad366452f5f1c6483a30be14fb2f27a39d5
Ricardo Taipe <rdavid28@hotmail.es>
2022-04-04 21:10:00 -0500

    add servicelocator

--- app/src/main/java/com/example/todoapp/TodoApplication.kt
+++ app/src/main/java/com/example/todoapp/TodoApplication.kt
@@ -2,12 +2,16 @@ package com.example.todoapp
 
 import android.app.Application
 import androidx.viewbinding.BuildConfig
+import com.example.todoapp.data.source.TasksRepository
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
-class TodoApplication: Application() {
+class TodoApplication : Application() {
+    val taskRepository: TasksRepository
+        get() = ServiceLocator.provideTasksRepository(this)
+
     override fun onCreate() {
         super.onCreate()
-        if(BuildConfig.DEBUG) Timber.plant(DebugTree())
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
     }
 }
