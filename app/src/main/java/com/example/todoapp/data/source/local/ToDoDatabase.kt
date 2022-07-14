9a06dfe4644070583fc12e11141c925326f9ef18
Ricardo Taipe <rdavid28@hotmail.es>
2022-07-14 13:00:52 -0500

    remove unnecessary todo

--- app/src/main/java/com/example/todoapp/data/source/local/ToDoDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/source/local/ToDoDatabase.kt
@@ -3,7 +3,7 @@ package com.example.todoapp.data.source.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.todoapp.data.Task
-//TODO search info about migration
+
 @Database(entities = [Task::class], version = 2, exportSchema = false)
 abstract class ToDoDatabase : RoomDatabase() {
     abstract fun taskDao(): TasksDao
