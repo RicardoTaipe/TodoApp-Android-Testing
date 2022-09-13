c7d4617f591be1980fe3561138b5333cfc90ea92
Ricardo Taipe <rdavid28@hotmail.es>
2022-09-13 18:00:54 -0500

    update database version

--- app/src/main/java/com/example/todoapp/data/source/local/ToDoDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/source/local/ToDoDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.todoapp.data.Task
 
-@Database(entities = [Task::class], version = 2, exportSchema = false)
+@Database(entities = [Task::class], version = 3, exportSchema = false)
 abstract class ToDoDatabase : RoomDatabase() {
     abstract fun taskDao(): TasksDao
 }
