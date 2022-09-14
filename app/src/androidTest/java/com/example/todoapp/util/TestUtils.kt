cad5d4c0d0e94994e887136ab712638f9048ce0b
Ricardo Taipe <rdavid28@hotmail.es>
2022-09-14 15:42:47 -0500

    updata Navigation Test

---
+++ app/src/androidTest/java/com/example/todoapp/util/TestUtils.kt
@@ -0,0 +1,16 @@
+package com.example.todoapp.util
+
+import android.app.Activity
+import androidx.appcompat.widget.Toolbar
+import androidx.test.core.app.ActivityScenario
+import com.example.todoapp.R
+
+fun <T : Activity> ActivityScenario<T>.getToolbarNavigationContentDescription()
+        : String {
+    var description = ""
+    onActivity {
+        description =
+            it.findViewById<Toolbar>(R.id.toolbar).navigationContentDescription as String
+    }
+    return description
+}
