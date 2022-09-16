a5eb3bb1ec31c5a4542321648b6debc7ee99fd6e
Ricardo Taipe <rdavid28@hotmail.es>
2022-09-16 10:54:19 -0500

    update test util
    (cherry picked from commit 679907c8236e751ca81df6e698c0442ba13f8b4f)

--- app/src/androidTest/java/com/example/todoapp/util/TestUtils.kt
+++ app/src/androidTest/java/com/example/todoapp/util/TestUtils.kt
@@ -1,9 +1,14 @@
 package com.example.todoapp.util
 
 import android.app.Activity
+import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.matcher.BoundedMatcher
 import com.example.todoapp.R
+import com.google.android.material.progressindicator.LinearProgressIndicator
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 
 fun <T : Activity> ActivityScenario<T>.getToolbarNavigationContentDescription()
         : String {
@@ -13,4 +18,17 @@ fun <T : Activity> ActivityScenario<T>.getToolbarNavigationContentDescription()
             it.findViewById<Toolbar>(R.id.toolbar).navigationContentDescription as String
     }
     return description
+}
+
+fun withProgress(expectedProgress: Int): Matcher<View> {
+    return object : BoundedMatcher<View, LinearProgressIndicator>(LinearProgressIndicator::class.java) {
+        public override fun matchesSafely(view: LinearProgressIndicator): Boolean {
+            return view.progress == expectedProgress
+        }
+
+        override fun describeTo(description: Description) {
+            description.appendText("expected: ")
+            description.appendText("" + expectedProgress)
+        }
+    }
 }
